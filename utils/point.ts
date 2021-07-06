70172c250051d09eae46fdd2fb3e84e308ac24a1
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2021-07-06 18:47:29 +0430

    add slop and middle

--- utils/point.ts
+++ utils/point.ts
@@ -1,89 +1,98 @@
 export default class Point {
-  x: number;
-  y: number;
+	x: number
+	y: number
 
-  constructor(x: number = 0, y: number = 0) {
-    this.x = x;
-    this.y = y;
-  }
+	constructor(x: number = 0, y: number = 0) {
+		this.x = x
+		this.y = y
+	}
 
-  static ZERO(): Point {
-    return new Point(0, 0);
-  }
+	static ZERO(): Point {
+		return new Point(0, 0)
+	}
 
-  static ONE(): Point {
-    return new Point(1, 1);
-  }
+	static ONE(): Point {
+		return new Point(1, 1)
+	}
 
-  static CONST_ZERO = Point.ZERO();
-  static CONST_ONE = Point.ONE();
+	static middle(p1: Point, p2: Point): Point {
+		return new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
+	}
 
-  set(x: number, y: number): Point {
-    this.x = x;
-    this.y = y;
-    return this;
-  }
+	static slope(p1: Point, p2: Point): number {
+		const m = (p2.y - p1.y) / (p2.x - p1.x)
+		return isNaN(m) ? 0 : m
+	}
 
-  clear(): void {
-    this.x = 0;
-    this.y = 0;
-  }
+	static CONST_ZERO = Point.ZERO()
+	static CONST_ONE = Point.ONE()
 
-  add(vector: Point): Point {
-    this.x += vector.x;
-    this.y += vector.y;
-    return this;
-  }
+	set(x: number, y: number): Point {
+		this.x = x
+		this.y = y
+		return this
+	}
 
-  sub(vector: Point): Point {
-    this.x -= vector.x;
-    this.y -= vector.y;
-    return this;
-  }
+	clear(): void {
+		this.x = 0
+		this.y = 0
+	}
 
-  multiply(scalar: number): Point {
-    this.x *= scalar;
-    this.y *= scalar;
-    return this;
-  }
+	add(vector: Point): Point {
+		this.x += vector.x
+		this.y += vector.y
+		return this
+	}
 
-  divide(scalar: number): Point {
-    this.x /= scalar;
-    this.y /= scalar;
-    return this;
-  }
+	sub(vector: Point): Point {
+		this.x -= vector.x
+		this.y -= vector.y
+		return this
+	}
 
-  normalize(): Point {
-    const mag = this.magnitude();
-    if (mag === 0) {
-      return Point.ZERO();
-    }
-    return this.divide(mag);
-  }
+	multiply(scalar: number): Point {
+		this.x *= scalar
+		this.y *= scalar
+		return this
+	}
 
-  magnitude(): number {
-    return Math.sqrt(this.x * this.x + this.y * this.y);
-  }
+	divide(scalar: number): Point {
+		this.x /= scalar
+		this.y /= scalar
+		return this
+	}
 
-  squaredMagnitude(): number {
-    return this.x * this.x + this.y * this.y;
-  }
+	normalize(): Point {
+		const mag = this.magnitude()
+		if (mag === 0) {
+			return Point.ZERO()
+		}
+		return this.divide(mag)
+	}
 
-  limit(max: number): Point {
-    const mSq = this.squaredMagnitude();
-    if (mSq > max * max) this.divide(Math.sqrt(mSq)).multiply(max);
-    return this;
-  }
+	magnitude(): number {
+		return Math.sqrt(this.x * this.x + this.y * this.y)
+	}
 
-  distance(vector: Point): number {
-    return Math.sqrt((this.x - vector.x) ** 2 + (this.y - vector.y) ** 2);
-  }
+	squaredMagnitude(): number {
+		return this.x * this.x + this.y * this.y
+	}
 
-  angle(): number {
-    return Math.atan2(this.x, this.y);
-  }
+	limit(max: number): Point {
+		const mSq = this.squaredMagnitude()
+		if (mSq > max * max) this.divide(Math.sqrt(mSq)).multiply(max)
+		return this
+	}
 
-  clone(): Point {
-    return new Point(this.x, this.y);
-  }
+	distance(vector: Point): number {
+		return Math.sqrt((this.x - vector.x) ** 2 + (this.y - vector.y) ** 2)
+	}
+
+	angle(): number {
+		return Math.atan2(this.x, this.y)
+	}
+
+	clone(): Point {
+		return new Point(this.x, this.y)
+	}
 }
