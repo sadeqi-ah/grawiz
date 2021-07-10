11d147114f38511f656b9912c4f6f9ec2b4a7868
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2021-07-11 04:02:54 +0430

    create Toolbox ColorPicker

---
+++ components/toolbox/ColorPicker.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import styles from '../../styles/Toolbox.module.scss'
+
+export type ColorPickerProps = {
+	colors: string[]
+	active: string
+	onUpdate?: (active: string) => void
+}
+
+const ColorPicker: React.FC<ColorPickerProps> = ({ colors, onUpdate, active }) => {
+	const [_active, setActive] = useState<string | undefined>(active)
+	const [hover, setHover] = useState<string | undefined>()
+
+	return (
+		<div className={styles.colorPicker}>
+			{colors.map(color => (
+				<div
+					key={color}
+					className={styles.color}
+					style={{
+						background: `${color}${_active === color || hover === color ? '' : '5E'}`,
+						border: `2px solid ${color}`,
+					}}
+					onMouseEnter={() => setHover(color)}
+					onMouseLeave={() => setHover(undefined)}
+					onClick={() => setActive(color)}
+				></div>
+			))}
+		</div>
+	)
+}
+
+export default ColorPicker
--- components/toolbox/index.tsx
+++ components/toolbox/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../../styles/Toolbox.module.scss'
 import Button, { ButtonProps } from './Button'
+import ColorPicker, { ColorPickerProps } from './ColorPicker'
 import Input, { InputProps } from './Input'
 
 interface IToolboxComposition {
@@ -9,6 +10,7 @@ interface IToolboxComposition {
 	Group: React.FC
 	Input: React.FC<InputProps>
 	Line: React.FC
+	ColorPicker: React.FC<ColorPickerProps>
 }
 
 const Toolbox: React.FC<{ width: number }> & IToolboxComposition = ({ children, width }) => {
@@ -20,6 +22,7 @@ const Toolbox: React.FC<{ width: number }> & IToolboxComposition = ({ children, 
 }
 Toolbox.Button = Button
 Toolbox.Input = Input
+Toolbox.ColorPicker = ColorPicker
 
 const Row: React.FC = ({ children }) => <div className={styles.row}>{children}</div>
 Toolbox.Row = Row
