6952ba12407d94f6da7f4c434be98b68963e0ef6
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2021-07-28 23:34:03 +0430

    write 'typeEdge' funtion

--- hooks/useGraphEditor.ts
+++ hooks/useGraphEditor.ts
@@ -2,12 +2,13 @@ import { useContext, useCallback } from 'react'
 import { NODE_RADIUS } from '@constants'
 import { GraphEditorContext, GraphEditorDispatchContext } from '@providers/GraphEditorProvider'
 import Point from '@utils/shape/point'
+import { EdgeType } from '@components/graph/Edge'
 
 export function useGraphEditor() {
 	const state = useContext(GraphEditorContext)
 	const dispatch = useContext(GraphEditorDispatchContext)
 
-	const getNodeByLabel = useCallback((label: string) => state.nodes.find(node => node.label == label), [state.nodes])
+	const getNodeById = useCallback((id: string) => state.nodes.find(node => node.id == id), [state.nodes])
 
 	const getNodeByPosition = useCallback(
 		(position: Point) =>
@@ -23,5 +24,24 @@ export function useGraphEditor() {
 		[state.nodes]
 	)
 
-	return { state, dispatch, getNodeByLabel, getNodeByPosition }
+	const typeEdge: (source: string | undefined, target: string | undefined) => EdgeType | undefined = useCallback(
+		(source, target) => {
+			if (source == undefined || target == undefined) return
+			const edges = state.edges.filter(
+				edge =>
+					(edge.source.id == source && edge.target.id == target) ||
+					(edge.source.id == target && edge.target.id == source)
+			)
+
+			if (edges.length >= 3) return
+			const edgeTypes = edges.map(edge => edge.type)
+			if (!edgeTypes.includes('straight')) return 'straight'
+			if (!edgeTypes.includes('curve')) return 'curve'
+			if (!edgeTypes.includes('reverse-curve')) return 'reverse-curve'
+			return undefined
+		},
+		[state.edges]
+	)
+
+	return { state, dispatch, getNodeById, getNodeByPosition, typeEdge }
 }
