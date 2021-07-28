c9271d7766d58f7852fe3f019a6ae08d040361b5
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2021-07-28 23:16:27 +0430

    add 'id' to InputProps

--- components/toolbox/Input.tsx
+++ components/toolbox/Input.tsx
@@ -1,21 +1,20 @@
-import React, { ChangeEvent, useEffect, useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import styles from '@styles/Toolbox.module.scss'
 
 export type InputProps = {
+	id: string | undefined
 	label: string
 	type: 'text' | 'number'
-	onChange?: (value: string | number | readonly string[] | undefined) => void
-} & Omit<JSX.IntrinsicElements['input'], 'onChange' | 'type'>
+	value?: string | number
+	onChange?: (id: string, value?: string | number) => void
+} & Omit<JSX.IntrinsicElements['input'], 'onChange' | 'type' | 'value'>
 
-const Input: React.FC<InputProps> = ({ label, value, onChange, type, ...props }) => {
-	const [state, setstate] = useState(value)
-
-	useEffect(() => {
-		if (onChange) onChange(state)
-	}, [state])
+const Input: React.FC<InputProps> = ({ id, label, value, onChange, type, ...props }) => {
+	const [state, setstate] = useState(value !== undefined ? value : '')
 
 	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setstate(e.target.value)
+		if (onChange && id) onChange(id, e.target.value)
 	}
 
 	return (
