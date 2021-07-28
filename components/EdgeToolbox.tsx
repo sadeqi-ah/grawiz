b9fd04854091f188a714246a71d3f8e45a9304b7
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2021-07-28 23:24:30 +0430

    complete toolbox

--- components/EdgeToolbox.tsx
+++ components/EdgeToolbox.tsx
@@ -1,34 +1,112 @@
-import React from 'react'
-import Toolbox, { ToolboxProps } from './toolbox'
+import React, { memo } from 'react'
+import Line from '@utils/shape/line'
+import Point from '@utils/shape/point'
+import Quadbezier from '@utils/shape/quadbezier'
+import { calcEdgePosition, GraphEdge } from './graph/Edge'
+import Toolbox from './toolbox'
+import isEqual from 'lodash/isEqual'
 
-export type EdgeToolboxProps = Omit<ToolboxProps, 'width'>
+export type EdgeToolboxProps = {
+	edge?: GraphEdge
+	onChangeDirection?: (id: string, direction: string[]) => void
+	onChangeEdgeType?: (id: string, type: string) => void
+	onChangeWeight?: (id: string, weight?: string | number) => void
+	defaultValue?: {
+		direction?: string[]
+		type?: string
+		edgeWeight?: number
+	}
+}
 
-const EdgeToolbox: React.FC<EdgeToolboxProps> = ({ position, show }) => {
+const EdgeToolbox: React.FC<EdgeToolboxProps> = ({
+	edge,
+	onChangeDirection,
+	onChangeEdgeType,
+	onChangeWeight,
+	defaultValue,
+}) => {
+	const calcEdgeToolboxPosition = (width: number, height: number) => {
+		if (!edge) return
+		const { first, control, last } = calcEdgePosition(
+			edge.source.position.clone().add(edge.source.translate),
+			edge.target.position.clone().add(edge.target.translate),
+			edge.type
+		)
+
+		const line = new Line(first, last)
+		const c = line.getPointByDistOnBisector(70)
+
+		const _position = control ? new Quadbezier(first, control, last).getPoint(0.5) || line.middle() : line.middle()
+		const _translate = Point.ZERO()
+
+		const angle = Math.atan(line.slope())
+		const p = (Math.abs(angle) * 50) / (Math.PI / 2)
+
+		if (line.slope() <= 0) {
+			_position.sub(c)
+			_translate.x = (-1 * (p + 50) * width) / 100
+		} else {
+			_position.add(c)
+			_translate.x = (-1 * (50 - p) * width) / 100
+		}
+		_translate.y = (-1 * (100 - p) * height) / 100
+
+		_position.add(_translate)
+
+		return _position
+	}
+
+	const position = calcEdgeToolboxPosition(240, 141)
+
+	const activeDirection = () => {
+		if (!edge || (edge && edge.direction === 'none')) return []
+		if (edge.direction === 'both') return ['start', 'end']
+		return [edge.direction]
+	}
+
 	return (
-		<Toolbox width={240} position={position} show={show}>
+		<Toolbox width={240} height={141} position={position}>
 			<Toolbox.Row>
 				<Toolbox.Group>
-					<Toolbox.Button type='check-box' icon={'ArrowLeft'} active={false} />
-					<Toolbox.Button type='check-box' icon={'ArrowRight'} active={false} />{' '}
+					<Toolbox.Button
+						id={edge?.id}
+						type='check-box'
+						options={[
+							{ id: 'start', icon: 'ArrowLeft' },
+							{ id: 'end', icon: 'ArrowRight' },
+						]}
+						onUpdate={onChangeDirection}
+						active={activeDirection()}
+					/>
 				</Toolbox.Group>
 				<Toolbox.Group>
 					<Toolbox.Button
-						active={'straight'}
+						id={edge?.id}
 						type='radio-button'
 						options={[
 							{ id: 'straight', icon: 'Line' },
-							{ id: 'upward-curve', icon: 'UpwardCurve' },
-							{ id: 'downward-curve', icon: 'DownwardCurve' },
+							{ id: 'curve', icon: 'UpwardCurve' },
+							{ id: 'reverse-curve', icon: 'DownwardCurve' },
 						]}
+						onChange={onChangeEdgeType}
+						active={edge?.type}
 					/>
 				</Toolbox.Group>
 			</Toolbox.Row>
 			<Toolbox.Line />
 			<Toolbox.Row>
-				<Toolbox.Input type={'number'} label='weight' value={10} />
+				<Toolbox.Group>
+					<Toolbox.Input
+						id={edge?.id}
+						type={'number'}
+						label='weight'
+						value={edge?.weight}
+						onChange={onChangeWeight}
+					/>
+				</Toolbox.Group>
 			</Toolbox.Row>
 		</Toolbox>
 	)
 }
 
-export default EdgeToolbox
+export default memo(EdgeToolbox, isEqual)
--- components/NodeToolbox.tsx
+++ components/NodeToolbox.tsx
@@ -1,23 +1,46 @@
-import React from 'react'
-import { COLORS } from '@constants'
-import Toolbox, { ToolboxProps } from './toolbox'
+import React, { memo } from 'react'
+import { COLORS, NODE_RADIUS } from '@constants'
+import Toolbox from './toolbox'
+import { GraphNode } from './graph/Node'
+import Point from '@utils/shape/point'
+import isEqual from 'lodash/isEqual'
 
-export type NodeToolboxProps = Omit<ToolboxProps, 'width'>
+export type NodeToolboxProps = {
+	node?: GraphNode
+	onChangeNodeColor?: (id: string, color: string) => void
+	onChangeNodeLabel?: (id: string, label?: string | number) => void
+}
 
-const NodeToolbox: React.FC<NodeToolboxProps> = ({ position, show }) => {
+const NodeToolbox: React.FC<NodeToolboxProps> = ({ node, onChangeNodeColor, onChangeNodeLabel }) => {
+	const toolboxPos = (width: number, height: number) => {
+		if (!node) return
+		return new Point(width / -2, -1 * NODE_RADIUS - 20 - height).add(node.position).add(node.translate)
+	}
+
 	return (
-		<Toolbox width={246} position={position} show={show}>
+		<Toolbox width={246} height={147} position={toolboxPos(246, 147)}>
 			<Toolbox.Row>
 				<Toolbox.Group>
-					<Toolbox.ColorPicker colors={COLORS} active={'#F45890'} />
+					<Toolbox.ColorPicker
+						id={node?.id}
+						colors={COLORS}
+						active={node?.color}
+						onUpdate={onChangeNodeColor}
+					/>
 				</Toolbox.Group>
 			</Toolbox.Row>
 			<Toolbox.Line />
 			<Toolbox.Row>
-				<Toolbox.Input type={'text'} label='label' value={10} />
+				<Toolbox.Input
+					id={node?.id}
+					type={'text'}
+					label='label'
+					value={node?.label}
+					onChange={onChangeNodeLabel}
+				/>
 			</Toolbox.Row>
 		</Toolbox>
 	)
 }
 
-export default NodeToolbox
+export default memo(NodeToolbox, isEqual)
