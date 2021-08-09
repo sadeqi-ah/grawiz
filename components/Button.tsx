6baccf454f7ab01fcec2812c5a62fbf95a8ac64e
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2021-08-09 13:42:48 +0430

    ♻️  refactor

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,30 +2,97 @@ import React, { useState } from 'react'
 import I from '@icons'
 import styles from '@styles/Button.module.scss'
 import classNames from '@utils/classNames'
+import { useUpdateEffect } from '@hooks/useUpdateEffect'
+
+export type ButtonState = boolean | string[]
+
+export type Option = { id: string; icon: string }
 
 export type ButtonProps = {
-	icon: string
-	switchMode?: boolean
-	status?: boolean
-	onClick?: (status: boolean) => void
+	id?: string
+	icon?: string
+	type?: 'normal' | 'checkbox' | 'radio'
+	options?: Option[]
+	status?: ButtonState
+	onClick?: () => void
+	changeState?: (state: ButtonState) => ButtonState
+	onUpdate?: (id: string, state: ButtonState) => void
 }
 
-const Button: React.FC<ButtonProps> = ({ icon, onClick, switchMode, status }) => {
-	const [state, setState] = useState(false)
-	const _status = status !== undefined ? status : state
+function Button({
+	icon,
+	onClick,
+	status,
+	type = 'normal',
+	options,
+	id,
+	changeState = (s: ButtonState) => s,
+	onUpdate,
+}: ButtonProps) {
+	const [state, setState] = useState<ButtonState>(() => {
+		if (status) return status
+		if ((type === 'checkbox' && options) || type === 'radio') return []
+		return false
+	})
 
-	const handleClick = () => {
-		if (switchMode && status !== undefined) setState(prev => !prev)
-		setTimeout(() => {
-			if (onClick) onClick(_status)
-		}, 0)
+	useUpdateEffect(() => {
+		if (onUpdate && id) onUpdate(id, state)
+	}, [state])
+
+	const handleClick = (_id?: string) => {
+		let newState: ButtonState
+
+		if (type === 'normal' && onClick) onClick()
+		if (type === 'radio' && _id) setState(changeState([_id]))
+
+		if (type === 'checkbox') {
+			setState(prev => {
+				if (typeof prev === 'object' && _id) {
+					newState = prev.slice()
+					if (newState.includes(_id)) newState.splice(newState.indexOf(_id), 1)
+					else newState.push(_id)
+				} else {
+					newState = !prev
+				}
+
+				return changeState(newState)
+			})
+		}
 	}
 
-	return (
-		<div className={classNames(styles.button, { [styles.active]: _status })} onClick={() => handleClick()}>
-			<I name={icon} width={26} height={26} />
-		</div>
-	)
+	const isActive = (_id?: string) => {
+		if (type === 'normal') return false
+		if (typeof state === 'boolean') return state
+		if (typeof state === 'object' && _id) {
+			return state.includes(_id)
+		}
+
+		return false
+	}
+
+	if (type === 'normal' || (!options && !id)) {
+		return (
+			<div className={classNames(styles.button, { [styles.active]: isActive() })} onClick={() => handleClick()}>
+				<I name={icon} width={26} height={26} />
+			</div>
+		)
+	} else {
+		console.log(type)
+		return (
+			<div className={styles.container}>
+				{options &&
+					options.map(option => (
+						<div
+							key={option.id}
+							className={classNames(styles.button, { [styles.active]: isActive(option.id) })}
+							onClick={() => handleClick(option.id)}
+						>
+							<I name={option.icon} width={26} height={26} />
+						</div>
+					))}
+			</div>
+		)
+	}
 }
 
 export default Button
