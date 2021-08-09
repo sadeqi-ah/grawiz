404e43d89c44092dc5608ed518786c79ccc52bb5
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2021-08-09 13:46:07 +0430

    🚚 move

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useState } from 'react'
-import styles from '@styles/Toolbox.module.scss'
+import styles from '@styles/Input.module.scss'
 
 export type InputProps = {
 	id: string | undefined
