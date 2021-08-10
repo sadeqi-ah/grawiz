0dc9f86fa172b071a48eb1016945b0a8eb18d045
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2021-08-10 19:30:26 +0430

    create Code component

---
+++ components/Code.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from '@styles/Code.module.scss'
+import { copy } from '@utils/copyTextToClipboard'
+import { useAlertDispatch } from '@providers/AlertProvider'
+
+export type CodeProps = {
+	code: string[]
+}
+
+function Code({ code }: CodeProps) {
+	const alert = useAlertDispatch()
+
+	const handleClick = () => {
+		copy(code.join('\n'))
+		alert('The text was successfully copied to the clipboard')
+	}
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.code}>
+				{code.map((line, index) => (
+					<p key={`code_line_${index}`}>{line}</p>
+				))}
+			</div>
+			<div className={styles.copy} onClick={() => handleClick()}>
+				copy
+			</div>
+		</div>
+	)
+}
+
+export default Code
