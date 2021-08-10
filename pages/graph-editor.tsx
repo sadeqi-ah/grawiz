6cf0357d190aacf87b1106ad5e342c38cf6c8a3b
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2021-08-10 19:43:13 +0430

    remove providers and add logo and export box

--- pages/graph-editor.tsx
+++ pages/graph-editor.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import { GraphEditorProvider } from '@providers/GraphEditorProvider'
+import React, { useState } from 'react'
 import GraphEditorComponent from '@components/graphEditor'
+import Logo from '@components/Logo'
+import Button from '@components/Button'
+import styles from '@styles/GraphEditor.module.scss'
+import ExportBox from '@components/ExportBox'
+import Alert from '@components/Alert'
+import { useAlert } from '@providers/AlertProvider'
 
 const GraphEditor = () => {
+	const [showExportBox, setShowExportBox] = useState(false)
+	const alert = useAlert()
+
 	return (
-		<GraphEditorProvider>
+		<>
 			<GraphEditorComponent />
-		</GraphEditorProvider>
+			<Logo />
+			<div className={styles.rightButtons}>
+				<Button
+					icon='Paper'
+					type='checkbox'
+					changeState={state => {
+						let newState = false
+						setShowExportBox(prev => {
+							newState = !prev
+							return !prev
+						})
+						return newState
+					}}
+				/>
+				<ExportBox show={showExportBox} />
+				<Alert text={alert.text} show={alert.show} />
+			</div>
+		</>
 	)
 }
 
