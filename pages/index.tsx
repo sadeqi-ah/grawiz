9a74c80231a6f5446a072bd312a5a1fa9626224b
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2021-08-29 14:45:00 +0430

    move app to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import Head from 'next/head'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-import Nav from '../components/Nav'
-import PostList from '../components/PostList'
 import styles from '../styles/Home.module.scss'
+import React, { useState } from 'react'
+import GraphEditorComponent from '@components/graphEditor'
+import Logo from '@components/Logo'
+import Button from '@components/Button'
+import ExportBox from '@components/ExportBox'
+import Alert from '@components/Alert'
+import { useAlert } from '@providers/alert'
 
 export default function Home() {
+	const [showExportBox, setShowExportBox] = useState(false)
+	const alert = useAlert()
+
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -14,11 +20,24 @@ export default function Home() {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<Nav />
-			<Header />
-			<PostList />
-
-			<Footer />
+			<GraphEditorComponent />
+			<Logo />
+			<div className={styles.rightButtons}>
+				<Button
+					icon='Paper'
+					type='checkbox'
+					changeState={state => {
+						let newState = false
+						setShowExportBox(prev => {
+							newState = !prev
+							return !prev
+						})
+						return newState
+					}}
+				/>
+				<ExportBox show={showExportBox} />
+				<Alert text={alert.text} show={alert.show} />
+			</div>
 		</div>
 	)
 }
